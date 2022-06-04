36eb1892f488211c2bbaa760df567f7fc369cc29
Student <Student@itstep18.org>
2022-06-04 10:53:42 +0400

    css was changed, component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Modal from './components/modal'
 
 function App() {
   return (
     <div className="App">
-      опа-па
+      <Modal></Modal>
     </div>
   );
 }
